757fa9c17603a378ff44ccfe05d662ddae638396
缐英达 <daegis@yeah.net>
2018-01-23 11:17:02 +0800

    Initial commit

---
+++ acm-provider/src/main/java/cn/aegisa/acm/web/controller/TestController.java
@@ -0,0 +1,35 @@
+package cn.aegisa.acm.web.controller;
+
+import cn.aegisa.acm.dao.service.ICommonService;
+import cn.aegisa.acm.model.City;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Using IntelliJ IDEA.
+ *
+ * @author HNAyd.xian
+ * @date 2018/1/22 12:26
+ */
+@Slf4j
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private ICommonService service;
+
+    @RequestMapping("/01/{id}")
+    public String test01(@PathVariable Integer id, Model model) {
+        log.info("访问到了这个方法");
+        List<City> id1 = service.getList(City.class, "id", id);
+        model.addAttribute("name", id1.get(0).getName());
+        return "test";
+    }
+}
